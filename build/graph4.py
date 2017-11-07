133eabfe27950aeb4617af01a437aae2de8ea219
Cécilia Droulez <Cecilia@MacBook-Pro-de-Cecilia.local>
2017-11-07 11:00:55 +0100

    Files to create the plots

---
+++ build/graph4.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+data = np.genfromtxt('Exp4.csv')
+
+select = np.array([d for d in data if d[1]<30])
+data1 = select.transpose()
+pl.xlim(1000,1200)
+pl.scatter(data1[0],data1[1],marker=2,alpha=0.8,edgecolors='none');
+pl.show();
+
+n,bins,patches = pl.hist(data1[0],200,range=(1000,1200),normed=0,alpha=0.75)
+pl.show();
+	
