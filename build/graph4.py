9361c9fa289dab6247440e824a733257fc6bd279
Cécilia Droulez <Cecilia@vpn-253-003.epfl.ch>
2017-11-11 10:34:34 +0100

    Changed display of the graphs

--- build/graph4.py
+++ build/graph4.py
@@ -6,7 +6,7 @@ data = np.genfromtxt('Exp4.csv')
 select = np.array([d for d in data if d[1]<30])
 data1 = select.transpose()
 pl.xlim(1000,1200)
-pl.scatter(data1[0],data1[1],marker=2,alpha=0.8,edgecolors='none');
+pl.scatter(data1[0],data1[1],alpha=0.8,edgecolors='none');
 pl.show();
 
 n,bins,patches = pl.hist(data1[0],200,range=(1000,1200),normed=0,alpha=0.75)
